177c77bfb0a661cb934534860cdaa045d5b84d0b
Wiktor <73667681+MoonGuyPro@users.noreply.github.com>
2024-01-10 01:30:35 +0100

    feat: second map generator

--- Assets/Scripts/MapGeneratorScripts/New/NewLevelGeneratorInteractiveObjects.cs
+++ Assets/Scripts/MapGeneratorScripts/New/NewLevelGeneratorInteractiveObjects.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 /*
  To jest 2 część generowania poziomu!
@@ -15,42 +17,165 @@ public class NewLevelGeneratorInteractiveObjects : MonoBehaviour
     [SerializeField] private GameObject finishPoint;
     [SerializeField] private GameObject spawnPoint;
     [SerializeField] private GameObject fireAltar;
+    [SerializeField] [Range(1, 3)] private int fireAltarAmount;
     [SerializeField] private GameObject iceAltar;
+    [SerializeField] [Range(1, 3)] private int iceAltarAmount;
     [SerializeField] private GameObject healingPoint;
     [SerializeField] private GameObject coin;
 
     [HideInInspector] public bool stopGeneration;
+    
 
     [Header("Ground Variables")]    //Do sprawdzenia sąsiedztwa danego tilea
     [SerializeField] private LayerMask layerToCheck;
-    [SerializeField] private float checkDistance;
-    private GameObject leftNeighbour, rightNeighbour, topNeighbour, bottomNeighbour;
-    
+
     private NewLevelGeneratorTerrain generator;
+    private SpawnRooms[] objectsWithSpawnRooms;
     private GameObject firstRoom;
     private GameObject lastRoom;
-    
-    // Szukamy wolnych miejsc gdzie jest możliwość spawnu 
-    public List<Transform> spawnPlaces;     //miejsca w których możemy zespawnować obiekty
+
+    private Vector2 roomSize;   //Do określenia rozmiaru pokoju
+
+    private List<GameObject> shorterList;       //lista w której nie am pokoi w ktorych zostały zespawnowane obiekty, potrzebna przy ołtarzach by nie powtarzac pokoju
 
 
     private void Start()
     {
         generator = FindObjectOfType<NewLevelGeneratorTerrain>();
+        roomSize = new Vector2(9, 9);
+        stopGeneration = false;
     }
 
     private void Update()
     {
-        if (generator.stopGeneration)
+        
+        if (generator.stopGeneration && !stopGeneration)
         {
-            SetEntranceAndExit();
+            SetAllInteractiveObjects();
         }
     }
 
-    private void SetEntranceAndExit()
+    private bool mapGenerated()
     {
-        firstRoom = generator.generatedRoomsOnPath[0];
-        
-        lastRoom = generator.generatedRoomsOnPath[^1];
+        objectsWithSpawnRooms = FindObjectsOfType<SpawnRooms>();
+        if (objectsWithSpawnRooms.Length == 0)
+        {
+            return true;
+        }
+
+        return false;
     }
+
+    private void SetAllInteractiveObjects()
+    {
+        //Najpierw spawn gracza i koniec poziomu
+        firstRoom = generator.generatedRoomsOnPath[0];
+        SpawnObjectInRoom(firstRoom, spawnPoint);
+        lastRoom = generator.generatedRoomsOnPath[^1];
+        SpawnObjectInRoom(lastRoom, finishPoint);
+        
+        //Teraz ołtarze
+        List<GameObject> roomsList = generator.generatedRandomRooms;    //Bierzemy pokoje poza główną ścieżką gracza
+        if (roomsList.Count < fireAltarAmount + iceAltarAmount)         //Na wypadek jakby było za mało pokoi dostępnych poza główną ścieżką
+        {
+            roomsList.AddRange(generator.generatedRoomsOnPath);
+        }
+        
+        SpawnManySameTypeObjects(fireAltar, fireAltarAmount, roomsList, true);
+        
+        SpawnManySameTypeObjects(iceAltar, iceAltarAmount, shorterList, false);     //tutaj z inną ograniczoną listą o te pokoje w ktorych jest juz ołtarz
+
+
+        stopGeneration = true;
+    }
+
+    private void SpawnManySameTypeObjects(GameObject gameObject, int amount, List<GameObject> roomsList, bool shouldReturnList)
+    {
+        for (int i = 0; i <= amount; i++)       //Generujemy taką ilość jaką wybraliśmy w inspektorze
+        {
+            int index = Random.Range(0, roomsList.Count);       //Losujemy pokój w którym powstanie obiekt
+            GameObject randomRoom = roomsList[index];
+            SpawnObjectInRoom(randomRoom, gameObject);
+            roomsList.Remove(roomsList[index]);
+        }
+
+        if (shouldReturnList)
+            shorterList = roomsList;
+    }
+    
+
+    private void SpawnObjectInRoom(GameObject room, GameObject objectPrefab)
+    {
+        List<GameObject> groundTilesInRoom = FindGroundInRoom(room);
+        List<GameObject> placesToSpawnList = FindPlacesToSpawn(groundTilesInRoom);
+        Vector2 spawnPosition = getRandomPositionToSpawn(placesToSpawnList);
+        Instantiate(objectPrefab, spawnPosition, quaternion.identity);
+    }
+
+    private List<GameObject> FindGroundInRoom(GameObject room)       //Znajduje obiekty terenu w pokoju i dodaje do listy
+    {
+        Vector2 center = room.transform.position;
+
+        List<GameObject> groundTilesInRoom = new List<GameObject>();
+
+        Collider2D[] hits = Physics2D.OverlapBoxAll(center, roomSize, 0, layerToCheck);
+        
+        foreach (Collider2D hit in hits)
+        {
+            // Sprawdź, czy trafienie to nie ten sam obiekt
+            if (hit.gameObject != gameObject)
+            {
+                groundTilesInRoom.Add(hit.gameObject);
+            }
+        }
+        
+        // Znaleźliśmy obiekty ziemi, teraz szukamy wolnych miejsc
+        return groundTilesInRoom;
+    }
+
+    private List<GameObject> FindPlacesToSpawn(List<GameObject> groundTilesList)    //Mając liste terenu znajduje teren nad którym jest wolne miejsce i zwracam liste z tymi miejscami
+    {
+        List<GameObject> placesToSpawnList = new List<GameObject>();
+        
+        foreach (GameObject obj in groundTilesList)
+        {
+            var position = obj.transform.position;
+            GameObject check = CheckNeighbourAtPosition(new Vector2(position.x, position.y + 1));
+            if (check == null)
+            {
+                placesToSpawnList.Add(obj);
+            }
+            
+        }
+
+        if (placesToSpawnList.Count == 0)
+        {
+            Debug.Log("Brak miejsca");
+        }
+
+        return placesToSpawnList;
+
+    }
+    
+    GameObject CheckNeighbourAtPosition(Vector2 position)
+    {
+        Collider2D collider = Physics2D.OverlapPoint(position);
+        if (collider != null)
+        {
+            return collider.gameObject;
+        }
+        return null;
+    }
+
+    private Vector2 getRandomPositionToSpawn(List<GameObject> gameObjects)
+    {
+        Vector2 position = new Vector2();
+
+        int index = Random.Range(0, gameObjects.Count);
+        GameObject choosed = gameObjects[index];
+        position = new Vector2(choosed.transform.position.x, choosed.transform.position.y + 1); //dodaje ponieważ gameobject ma pozycje ziemi nad którą jest miejsce
+
+        return position;
+    }
+
 }
